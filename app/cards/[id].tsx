b224591dde645088fbaa9bb6bf229081bdf854c8
David Serrano <david-serrano@outlook.com>
2025-04-07 10:14:15 -0500

    agrego rutas y barra de navegacion

---
+++ app/cards/[id].tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const cardDetails = () => {
+  return (
+    <View>
+      <Text>Card Details</Text>
+    </View>
+  );
+};
+
+export default cardDetails;
