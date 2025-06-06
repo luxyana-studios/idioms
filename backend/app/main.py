8dde1d1c83e04182a0dd86817075c99486af19aa
Álvaro Troya <alvaro.troya.ec@googlemail.com>
2025-06-06 16:52:40 +0200

    Add upvotes and downvotes to idioms

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,13 @@
 from typing import Annotated
+from uuid import UUID
 
-from fastapi import Depends, FastAPI, Query
+from fastapi import Depends, FastAPI, HTTPException, Query
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 
 from app import database
 from app.models.idioms import IdiomModel
-from app.schemas.idioms import IdiomCreate, IdiomSchema
+from app.schemas.idioms import IdiomCreate, IdiomSchema, IdiomUpdate
 
 SessionDep = Annotated[Session, Depends(database.get_session)]
 
@@ -39,13 +40,18 @@ def get_idioms(
     if not search:
         return [
             IdiomSchema.model_validate(idiom)
-            for idiom in db.query(IdiomModel).limit(limit).offset(offset).all()
+            for idiom in db.query(IdiomModel)
+            .order_by(IdiomModel.text.asc())
+            .limit(limit)
+            .offset(offset)
+            .all()
         ]
     else:
         return [
             IdiomSchema.model_validate(idiom)
             for idiom in db.query(IdiomModel)
             .filter(IdiomModel.text.ilike(f"%{search}%"))
+            .order_by(IdiomModel.text.asc())
             .limit(limit)
             .offset(offset)
             .all()
@@ -57,3 +63,45 @@ def post_idioms(db: SessionDep, payload: list[IdiomCreate]) -> None:
     idioms = [IdiomModel(**idiom.model_dump()) for idiom in payload]
     db.add_all(idioms)
     db.commit()
+
+
+@app.post("/idioms/{id}/upvote", response_model=IdiomSchema)
+async def upvote_idiom(db: SessionDep, id: UUID) -> IdiomSchema:
+    idiom = db.query(IdiomModel).filter(IdiomModel.id == id).first()
+    if not idiom:
+        raise HTTPException(status_code=404, detail="Idiom not found")
+
+    idiom.upvotes += 1
+    db.add(idiom)
+    db.commit()
+    db.refresh(idiom)
+    return idiom
+
+
+@app.post("/idioms/{id}/downvote", response_model=IdiomSchema)
+async def downvote_idiom(db: SessionDep, id: UUID) -> IdiomSchema:
+    idiom = db.query(IdiomModel).filter(IdiomModel.id == id).first()
+    if not idiom:
+        raise HTTPException(status_code=404, detail="Idiom not found")
+
+    idiom.downvotes += 1
+    db.add(idiom)
+    db.commit()
+    db.refresh(idiom)
+    return idiom
+
+
+@app.patch("/idioms/{id}", response_model=IdiomSchema)
+async def update_idiom(db: SessionDep, id: UUID, payload: IdiomUpdate) -> IdiomSchema:
+    idiom = db.query(IdiomModel).filter(IdiomModel.id == id).first()
+    if not idiom:
+        raise HTTPException(status_code=404, detail="Idiom not found")
+
+    idiom_update = payload.model_dump(exclude_unset=True)
+    for key, value in idiom_update.items():
+        setattr(idiom, key, value)
+
+    db.add(idiom)
+    db.commit()
+    db.refresh(idiom)
+    return idiom
--- backend/app/models/idioms.py
+++ backend/app/models/idioms.py
@@ -1,6 +1,17 @@
 from uuid import uuid4
 
-from sqlalchemy import ARRAY, FLOAT, TEXT, TIMESTAMP, UUID, Column, String, func
+from sqlalchemy import (
+    ARRAY,
+    BOOLEAN,
+    FLOAT,
+    INTEGER,
+    TEXT,
+    TIMESTAMP,
+    UUID,
+    Column,
+    String,
+    func,
+)
 
 from app.database import Base
 
@@ -22,6 +33,9 @@ class IdiomModel(Base):
     depiction = Column(ARRAY(String))
     alternative_depiction = Column(ARRAY(String))
     meaning_depiction = Column(ARRAY(String))
+    favorite = Column(BOOLEAN, default=False)
+    upvotes = Column(INTEGER, default=0)
+    downvotes = Column(INTEGER, default=0)
 
     created_at = Column(TIMESTAMP, server_default=func.now())
     updated_at = Column(
